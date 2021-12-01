235eb2c5c2d620a42dd519cc9d2dcf35c0590295
Josh Anness <annessjosh@gmail.com>
2021-12-01 10:38:31 -0500

    Update to Home Controller

--- WSAFinalProject/WSAFinalProject/Controllers/HomeController.cs
+++ WSAFinalProject/WSAFinalProject/Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace WSAFinalProject.Controllers
 {
     public class HomeController : Controller
     {
+        private MovieContext context {get; set;}
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -18,9 +19,14 @@ namespace WSAFinalProject.Controllers
             _logger = logger;
         }
 
+        public HomeController(MovieContext ctx) {
+            context = ctx;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            var movies = context.Movies.OrderBy(m => m.Name).ToList();
+            return View(movies);
         }
 
 
