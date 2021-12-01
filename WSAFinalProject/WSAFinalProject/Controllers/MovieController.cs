a25083d48475371256337d8f63531af39cf029e5
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2021-12-01 15:01:15 -0500

    Added working edit & delete buttons for each movie entry Added working "Add Movie" page

--- WSAFinalProject/WSAFinalProject/Controllers/MovieController.cs
+++ WSAFinalProject/WSAFinalProject/Controllers/MovieController.cs
@@ -13,5 +13,65 @@ namespace MovieList.Controllers
             context = ctx;
         }
 
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Action = "Add";
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList();
+            ViewBag.Actors = context.Actors.OrderBy(a => a.Name).ToList();
+            ViewBag.Casts = context.Casts.OrderBy(c => c.CastId).ToList();
+            ViewBag.Descriptions = context.Descriptions.OrderBy(d => d.DescriptionId).ToList();
+            return View("Edit", new Movie());
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            ViewBag.Action = "Edit";
+            ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList();
+            ViewBag.Actors = context.Actors.OrderBy(a => a.Name).ToList();
+            ViewBag.Casts = context.Casts.OrderBy(c => c.CastId).ToList();
+            ViewBag.Descriptions = context.Descriptions.OrderBy(d => d.DescriptionId).ToList();
+            var movie = context.Movies.Find(id);
+            return View(viewName: "../Movie/Edit", movie);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie movie)
+        {
+            if (ModelState.IsValid)
+            {
+                if (movie.MovieId == 0)
+                    context.Movies.Add(movie);
+                else
+                    context.Movies.Update(movie);
+                context.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ViewBag.Action = (movie.MovieId == 0) ? "Add" : "Edit";
+                ViewBag.Genres = context.Genres.OrderBy(g => g.Name).ToList();
+                ViewBag.Actors = context.Actors.OrderBy(a => a.Name).ToList();
+                ViewBag.Casts = context.Casts.OrderBy(c => c.CastId).ToList();
+                ViewBag.Descriptions = context.Descriptions.OrderBy(d => d.DescriptionId).ToList();
+                return View(movie);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var movie = context.Movies.Find(id);
+            return View(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie movie)
+        {
+            context.Movies.Remove(movie);
+            context.SaveChanges();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
--- WSAFinalProject/WSAFinalProject/Models/Domain Models/Movie.cs
+++ WSAFinalProject/WSAFinalProject/Models/Domain Models/Movie.cs
@@ -16,11 +16,16 @@ namespace WSAFinalProject.Models
         [Required(ErrorMessage = "Please enter a year.")]
         [Range(1888, 3000, ErrorMessage = "Year must be after 1888.")]
         public int? Year { get; set; }
+        
+        [Required(ErrorMessage = "Please select a genre.")]
         public int GenreId { get; set; }
         public Genre Genre { get; set; }
 
+        [Required(ErrorMessage ="Please enter a duration in minutes.")]
+        [Range(1,400,ErrorMessage = "Duration must be between 1 and 400 minutes.")]
         public int Duration { get; set; }
 
+        [Required(ErrorMessage = "Please enter a description.")]
         public int DescriptionId { get; set; }
         public Description Description { get; set; }
 
