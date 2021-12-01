a11f749c68a0515f809c5d127ffa5bf16dc76253
Josh Anness <annessjosh@gmail.com>
2021-11-30 19:23:08 -0500

    Update to DB

--- WSAFinalProject/WSAFinalProject/Startup.cs
+++ WSAFinalProject/WSAFinalProject/Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -8,6 +9,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WSAFinalProject.Models;
 
 namespace WSAFinalProject
 {
@@ -24,6 +26,10 @@ namespace WSAFinalProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<MovieContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("MovieContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
