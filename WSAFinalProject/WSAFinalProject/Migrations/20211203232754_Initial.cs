56c89c4100c26b626408c492dc40d5cc8ac44887
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2021-12-03 18:40:41 -0500

    Removed Data Models for: Actor, ActorMovie, Cast, Description Removed associated config files, views, controllers Edits made to MovieContext and remaning views to accomodate changes

---
+++ WSAFinalProject/WSAFinalProject/Migrations/20211203232754_Initial.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WSAFinalProject.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    GenreId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.GenreId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    MovieId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: false),
+                    Year = table.Column<int>(nullable: false),
+                    GenreId = table.Column<int>(nullable: false),
+                    Duration = table.Column<int>(nullable: false),
+                    Description = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.MovieId);
+                    table.ForeignKey(
+                        name: "FK_Movies_Genres_GenreId",
+                        column: x => x.GenreId,
+                        principalTable: "Genres",
+                        principalColumn: "GenreId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "GenreId", "Name" },
+                values: new object[,]
+                {
+                    { 1, "Horror" },
+                    { 2, "SciFi" },
+                    { 3, "Drama" },
+                    { 4, "Comedy" },
+                    { 5, "RomCom" },
+                    { 6, "Action" },
+                    { 7, "SuperHero" },
+                    { 8, "Musical" },
+                    { 9, "Documentary" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Description", "Duration", "GenreId", "Name", "Year" },
+                values: new object[] { 1, "A rescue crew investigates a spaceship that disappeared into a black hole and has now returned...with someone or something new on-board.", 96, 2, "Event Horizon", 1997 });
+
+            migrationBuilder.InsertData(
+                table: "Movies",
+                columns: new[] { "MovieId", "Description", "Duration", "GenreId", "Name", "Year" },
+                values: new object[] { 2, "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose.", 127, 2, "Jurassic Park", 1993 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Movies_GenreId",
+                table: "Movies",
+                column: "GenreId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+        }
+    }
+}
--- WSAFinalProject/WSAFinalProject/Models/Configuration/MovieConfig.cs
+++ WSAFinalProject/WSAFinalProject/Models/Configuration/MovieConfig.cs
@@ -12,8 +12,8 @@ namespace WSAFinalProject.Models
         public void Configure(EntityTypeBuilder<Movie> entity)
         {
             entity.HasData(
-                    new Movie { MovieId = 1, Name = "Event Horizon", Year = 1997, GenreId = 2, Duration = 96, DescriptionId = 1, CastId = 1},
-                    new Movie { MovieId = 2, Name = "Jurassic Park", Year = 1993, GenreId = 2, Duration = 127, DescriptionId = 2, CastId = 2}
+                    new Movie { MovieId = 1, Name = "Event Horizon", Year = 1997, GenreId = 2, Duration = 96, Description = "A rescue crew investigates a spaceship that disappeared into a black hole and has now returned...with someone or something new on-board." },
+                    new Movie { MovieId = 2, Name = "Jurassic Park", Year = 1993, GenreId = 2, Duration = 127, Description = "A pragmatic paleontologist touring an almost complete theme park on an island in Central America is tasked with protecting a couple of kids after a power failure causes the park's cloned dinosaurs to run loose." }
                 );
         }
     }
