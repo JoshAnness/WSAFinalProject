5ab328114421644bdb6a8e0f455539490adb2f38
Nicholas-Lawson <lawsonnj@mail.uc.edu>
2021-12-02 23:42:26 -0500

    -Made modifications to setup controller and view for Nick's personal page

---
+++ WSAFinalProject/WSAFinalProject/Controllers/NickController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSAFinalProject.Models;
+
+namespace WSAFinalProject.Controllers
+{
+    public class NickController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public NickController(MovieContext ctx)
+        {
+            context = ctx;
+        } 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
