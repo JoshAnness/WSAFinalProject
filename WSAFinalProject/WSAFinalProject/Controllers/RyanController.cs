5e19aaa89629115af0db925f76e01d9c738ca95b
richrn <richrn@mail.uc.edu>
2021-12-04 10:01:55 -0500

    personal page push ryan rich
    personal page push ryan rich

---
+++ WSAFinalProject/WSAFinalProject/Controllers/RyanController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSAFinalProject.Models;
+
+namespace WSAFinalProject.Controllers
+{
+    public class HenryController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public HenryController(MovieContext ctx)
+        {
+            context = ctx;
+        } 
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
