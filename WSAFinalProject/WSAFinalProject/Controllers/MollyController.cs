4881b6aaccee21b6fa84f5535bd5c84b5ebd418a
M-Bischoff <93992306+M-Bischoff@users.noreply.github.com>
2021-12-03 19:07:44 -0500

    Added personal page and personal controller, updated _Layout to include my page

---
+++ WSAFinalProject/WSAFinalProject/Controllers/MollyController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WSAFinalProject.Models;
+
+namespace WSAFinalProject.Controllers
+{
+    public class MollyController : Controller
+    {
+        private MovieContext context { get; set; }
+
+        public MollyController(MovieContext ctx)
+        {
+            context = ctx;
+        }
+
+        public IActionResult Index()
+        {
+            var movies = new List<Movie>();
+            foreach (Movie m in context.Movies)
+            {
+                if (m.GenreId == 1)
+                {
+                    m.Genre = context.Genres.Find(m.GenreId);
+                    movies.Add(m);
+                }
+                else if (m.GenreId == 2)
+                {
+                    m.Genre = context.Genres.Find(m.GenreId);
+                    movies.Add(m);
+                }
+            }
+            return View(movies);
+        }
+    }
+}
